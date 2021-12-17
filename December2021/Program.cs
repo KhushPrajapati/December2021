17f59304bdad1d27b0cb466434d1aaf337fbb886
Prajapati <khushbu.prajapati91@gmail.com>
2021-12-17 20:03:31 +1100

    Create New Material Added

--- December2021/Program.cs
+++ December2021/Program.cs
@@ -1,6 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using OpenQA.Selenium;
+﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
 
@@ -39,6 +37,74 @@ namespace December2021 // Note: actual namespace depends on the project name.
             else
             {
                 Console.WriteLine("Login failed, test failed.");
+            }
+
+            // Create time and material record
+
+            // Go to time and material page
+
+            IWebElement administrationDropdown = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/a"));
+            administrationDropdown.Click();
+
+            IWebElement tmOption = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/ul/li[3]/a"));
+            tmOption.Click();
+
+
+            // Click on create new button
+
+            IWebElement createNewButton = driver.FindElement(By.XPath("//*[@id='container']/p/a"));
+            createNewButton.Click();
+
+            // Select typecode from Dropdown list
+
+            IWebElement typeCodeDropdown = driver.FindElement(By.XPath("//*[@id='TimeMaterialEditForm']/div/div[1]/div/span[1]/span/span[1]"));
+            typeCodeDropdown.Click();
+
+            IWebElement materialOption = driver.FindElement(By.XPath("//*[@id='TypeCode_listbox']"));
+            materialOption.Click();
+
+            // Identify the code textbox and input a code
+
+            IWebElement codeTextbox = driver.FindElement(By.Id("Code"));
+            codeTextbox.SendKeys("TDecember2021");
+
+            // Identify the Discription and input a description
+
+            IWebElement discriptionTextbox = driver.FindElement(By.Id("Description"));
+            discriptionTextbox.SendKeys("TDecember2021");
+
+            // Identify the Price textbox and input a price
+            IWebElement priceTag = driver.FindElement(By.XPath("//*[@id='TimeMaterialEditForm']/div/div[4]/div/span[1]/span/input[1]"));
+            priceTag.Click();
+
+            IWebElement priceTextbox = driver.FindElement(By.Id("Price"));
+            priceTextbox.SendKeys("12");
+
+
+            // Click on Save Button
+
+            IWebElement saveButton = driver.FindElement(By.Id("SaveButton"));
+            saveButton.Click();
+            Thread.Sleep(2000);
+
+
+            // go to last page button
+
+            IWebElement lastPageButton = driver.FindElement(By.XPath("//*[@id='tmsGrid']/div[4]/a[4]/span"));
+            lastPageButton.Click();
+            Thread.Sleep(2000);
+
+            // check if record created is present in the table and has expected value
+
+            IWebElement actualCode = driver.FindElement(By.XPath("//*[@id='tmsGrid']/div[3]/table/tbody/tr[last()]/td[1]"));
+            
+            if(actualCode.Text == "TDecember2021")
+            {
+                Console.WriteLine("Material record created Successfully, test passed.");
+            }
+            else 
+            {
+                Console.WriteLine("Test failed.");
             }
 
         }
